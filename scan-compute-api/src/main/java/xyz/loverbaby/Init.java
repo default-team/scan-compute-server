1eb5071ad21fdb2a78c48e9f69ef87e30b84ee3e
曹远成 <yccao01@mail.nfsq.com.cn>
2019-05-16 09:38:28 +0800

    init2

---
+++ scan-compute-api/src/main/java/xyz/loverbaby/Init.java
@@ -0,0 +1,10 @@
+package xyz.loverbaby;
+
+/**
+ * @Classname Init
+ * @Description TODO
+ * @Date 2019/5/16 上午9:30
+ * @Created by yccao
+ */
+public class Init {
+}
---
+++ scan-compute-impl/src/main/java/xyz/compute/ScanComputeApplication.java
@@ -0,0 +1,14 @@
+package xyz.compute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ScanComputeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScanComputeApplication.class,args);
+    }
+
+
+}
