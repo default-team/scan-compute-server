9609785fa77a0676a27d43d8c714de1d8505eee1
曹远成 <yccao01@mail.nfsq.com.cn>
2019-05-16 21:34:48 +0800

    init BuildRecord

--- scan-compute-impl/src/main/java/xyz/loverbaby/ScanComputeApplication.java
+++ scan-compute-impl/src/main/java/xyz/loverbaby/ScanComputeApplication.java
@@ -1,4 +1,4 @@
-package xyz.compute;
+package xyz.loverbaby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- scan-compute-impl/src/test/java/xyz/loverbaby/ScanComputeImplApplicationTests.java
+++ scan-compute-impl/src/test/java/xyz/loverbaby/ScanComputeImplApplicationTests.java
@@ -1,4 +1,4 @@
-package xyz.compute;
+package xyz.loverbaby;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
